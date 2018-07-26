30194d34b64d438a592a66b5110b596095035cda
riaanggraini99 <ria.anggraini@students.amikom.ac.id>
2018-07-27 03:22:45 +0700

    make it better

--- server/models/consult.js
+++ server/models/consult.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 const crypto = require('crypto');
 mongoose.connect('mongodb://localhost/itHealth');
 
-// mongoose.models = {};
-// mongoose.modelSchemas = {};
 
 const CaseSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
--- server/models/patient.js
+++ server/models/patient.js
@@ -55,7 +55,7 @@ const PatientSchema = new mongoose.Schema({
     type :String,
     index:true
 },
-med:{type: mongoose.Schema.ObjectId, ref: 'Medicaation',
+med:{type: mongoose.Schema.ObjectId, ref: 'Medication',
 index:true},
   photo: {
     // /index: true,
@@ -65,43 +65,8 @@ index:true},
   },
 });
 
-// PatientSchema.virtual('password')
-//   .set(function(password) {
-//     this._password = password;
-//     this.salt = this.makeSalt();
-//     this.hashed_password = this.encryptPassword(password);
-//   })
-//   .get(function() {
-//     return this._password;
-//   });
+const Patient = mongoose.model('Patient', PatientSchema,'patients' );
+const login = new Patient({ email: 'admin@admin.com', password: 'admin123' });
 
-// PatientSchema.path('hashed_password').validate(function(v) {
-//   if (this._password && this._password.length < 6) {
-//     this.invalidate('password', 'Password must be at least 6 characters.');
-//   }
-//   if (this.isNew && !this._password) {
-//     this.invalidate('password', 'Password is required');
-//   }
-// }, null);
-
-// PatientSchema.methods = {
-//   authenticate(plainText) {
-//     return this.encryptPassword(plainText) === this.hashed_password;
-//   },
-//   encryptPassword(password) {
-//     if (!password) return '';
-//     try {
-//       return crypto
-//         .createHmac('sha1', this.salt)
-//         .update(password)
-//         .digest('hex');
-//     } catch (err) {
-//       return '';
-//     }
-//   },
-//   makeSalt() {
-//     return `${Math.round(new Date().valueOf() * Math.random())}`;
-//   },
-// };
 
 module.exports = mongoose.model('Patient', PatientSchema,'patients' );
